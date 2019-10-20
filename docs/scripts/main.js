02c7dd29cff1bed8caa5dc6cd763db80e15a76f3
Jonathan Sharman <jonathan.sharman@gmail.com>
2019-10-19 22:23:26 -0500

    Improved layout, css.

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -7,41 +7,49 @@ document.addEventListener('click', function enableNoSleep() {
 	noSleep.enable();
 }, false);
 
-let military;
+let military = null;
 
 let alarmHour = null;
 let alarmMinute = null;
 let currentHour = null;
 let currentMinute = null;
 
+const hoursTable = document.getElementById("hours").tBodies[0];
+const minutesTable = document.getElementById("minutes").tBodies[0];
+const snoozeButton = document.getElementById("snooze");
+const stopButton = document.getElementById("stop");
+
 function twoDigit(n) {
 	return n < 10 ? "0" + n.toString() : n.toString();
 }
 
 function makeHoursTable() {
-	const hours = document.getElementById("hours").tBodies[0];
-	hours.innerHTML = "";
+	hoursTable.innerHTML = "";
 	for (let rowIdx = 0; rowIdx < 2; ++rowIdx) {
-		const row = hours.insertRow(rowIdx);
+		const row = hoursTable.insertRow(rowIdx);
 		for (let colIdx = 0; colIdx < 12; ++colIdx) {
 			const cell = row.insertCell(colIdx);
 			if (military) {
 				cell.innerHTML = twoDigit(12 * rowIdx + colIdx);
 			} else {
-				const hour = (colIdx == 0 ? "12" : colIdx.toString()) + (rowIdx == 0 ? "am" : "pm");
+				const hour = colIdx == 0 ? "12" : colIdx.toString();
 				cell.innerHTML = hour;
 			}
+		}
+		if (!military) {
+			const amPm = rowIdx == 0 ? "am" : "pm";
+			row.insertCell(12).innerHTML = amPm;
+			row.insertCell(0).innerHTML = amPm;
 		}
 	}
 }
 function makeMinutesTable() {
-	const minutes = document.getElementById("minutes").tBodies[0];
-	minutes.innerHTML = "";
-	let tens = minutes.insertRow(0);
+	minutesTable.innerHTML = "";
+	let tens = minutesTable.insertRow(0);
 	for (let colIdx = 0; colIdx < 6; ++colIdx) {
 		tens.insertCell(colIdx).innerHTML = colIdx;
 	}
-	const ones = minutes.insertRow(1);
+	const ones = minutesTable.insertRow(1);
 	for (let colIdx = 0; colIdx < 10; ++colIdx) {
 		ones.insertCell(colIdx).innerHTML = colIdx;
 	}
@@ -78,20 +86,25 @@ function updateCurrentTime() {
 	if (hoursLeft < 0) {
 		hoursLeft += 24;
 	}
-	document.getElementById("timeLeft").innerHTML = "-" + twoDigit(hoursLeft) + ":" + minutesLeft + ":" + twoDigit(secondsLeft);
+	document.getElementById("timeLeft").innerHTML = "-" + twoDigit(hoursLeft) + ":" + twoDigit(minutesLeft) + ":" + twoDigit(secondsLeft);
 };
 
 function updateAlarmTime() {
-	document.getElementById("alarmTime").innerHTML = alarmHour != null && alarmMinute != null
-		? timeString(alarmHour, alarmMinute)
-		: "OFF";
-}
-
-function updateTimeLeft() {
+	if (alarmHour != null && alarmMinute != null) {
+		var alarmTimeText = timeString(alarmHour, alarmMinute);
+		snoozeButton.disabled = false;
+		stopButton.disabled = false;
+	} else {
+		var alarmTimeText = "OFF";
+		snoozeButton.disabled = true;
+		stop.disabled = true;
+	}
+	document.getElementById("alarmTime").innerHTML = alarmTimeText;
 }
 
 function setMilitary(value) {
 	military = value;
+	localStorage.setItem("military", value);
 	makeHoursTable();
 	makeMinutesTable();
 	updateCurrentTime();
@@ -104,11 +117,17 @@ function toggleMilitary(checkbox) {
 
 setInterval(updateCurrentTime, 100);
 
-setMilitary(true);
-
 alarmHour = 12;
 alarmMinute = 10;
-updateAlarmTime();
+
+setMilitary((function() {
+	let military = localStorage.getItem("military");
+	if (military == null) {
+		localStorage.setItem("military", true);
+		return true;
+	}
+	return military;
+})());
 
 // Unhide body after building page.
 document.getElementsByTagName("body")[0].style.visibility = "visible";
